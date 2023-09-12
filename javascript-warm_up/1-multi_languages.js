c3796731923a63b3d0bf85aaae44c99f16da8363
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-12 07:54:32 -0700

    a program to print 3 Lines of code using js

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,16 @@
+// Write a script that prints 3 lines:
+
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+
+
+const lineOne = "C is fun";
+const lineTwo = "Python is cool";
+const lineThree = "JavaScript is amazing";
+
+console.log(lineOne);
+console.log(lineTwo);
+console.log(lineThree);
