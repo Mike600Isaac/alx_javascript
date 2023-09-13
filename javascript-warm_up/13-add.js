544c93622c125efc51108f7a5e17985398501080
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-13 05:14:01 -0700

    function that add two integers

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,12 @@
+// Write a function that returns the addition of 2 integers.
+
+// The function must be visible from outside
+// The name of the function must be add
+// You are not allowed to use var
+
+
+function add(a, b){
+  return a + b
+};
+
+console.log(add(3,5))
