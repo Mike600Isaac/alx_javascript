263bb54c1d1541fdd74ce235ac354e74427f437b
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-06 07:43:10 -0700

    Write a class Rectangle that defines a rectangle:

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+// Write a class Rectangle that defines a rectangle:
+class Rectangle{
+
+  constructor(w,h) {
+    
+    if((w <= 0) || (h <= 0) || !w || !h){
+      return this
+    }
+    this.width = w;
+    this.height = h;
+
+  }
+  print () {
+    for (let i = 0; i <= this.height; i++) {
+      let row = '';
+      for (let j = 0; j <= this.width; j++) {
+          row += 'X';
+      }
+      console.log(row)
+    }
+  }
+}
+
+module.exports = Rectangle
+
+
+
+// function print (width,height) {
+//   for (let i = 0; i <= height; i++) {
+//     let row = '';
+//     for (let j = 0; j <= width; j++) {
+//         row += 'X';
+//     }
+//     console.log(row);
+//   }
+// }
+
+// print(3,4);
