a40ec587c0a05d0a63b90edb96edb24b522191a3
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-06 07:10:24 -0700

    create class using dot notation & an empty object

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,31 +3,31 @@
 // create a class using a dot notation & an empty object
 // using the prototype
 
-// class Rectangle {
-//   constructor(w,h) {
-
-//     if( (w || h === 0) || (w || h < 0) ){ 
-
-//     }
-
-//     this.width = w;
-//     this.height = h;
-//   }
-  
-// }
-
-
-
-// module.exports = Rectangle;
-
-
-
-
-
-
 class Rectangle {
+  constructor(w,h) {
 
-	constructor(w, h) {
+    if( (w || h === 0) || (w || h < 0) ){ 
+      return this;
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+  
+}
+
+
+
+module.exports = Rectangle;
+
+
+
+
+
+
+// class Rectangle {
+
+// 	constructor(w, h) {
 		// if ((w <= 0) || (h <= 0) || !Number.isInteger(w) || !Number.isInteger(h)) {
 		// 	this.width = undefined;
 		// 	this.height = undefined;
@@ -36,12 +36,12 @@ class Rectangle {
 		// 	this.width = w;
 		// 	this.width = h;
 		// }
-		if ((w <= 0) || (h <= 0) || !h || !w) {
-            return this;
-          }
-        this.width = w;
-        this.height = h;
-	}
-}
+// 		if ((w <= 0) || (h <= 0) || !h || !w) {
+//             return this;
+//           }
+//         this.width = w;
+//         this.height = h;
+// 	}
+// }
 
-module.exports = Rectangle;
+// module.exports = Rectangle;
