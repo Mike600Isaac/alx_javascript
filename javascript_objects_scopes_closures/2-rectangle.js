d529746e330beddacc407ae787dbbd6c417139c0
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-06 06:41:45 -0700

    create class using dot notation & an empty object

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+//#!/usr/bin/node
+// create a class using a dot notation & an empty object
+
+class Rectangle {
+  constructor(w,h) {
+
+    this.width = w;
+    this.height = h;
+  }
+  
+}
+
+if( width || height === 0 || width || height < 0 ){
+  const objectA = new object(); 
+}
+
+module.exports = Rectangle
