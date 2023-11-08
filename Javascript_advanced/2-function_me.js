751b1f8fb3d961cb7cb01066241f6815ce3b8fbb
Micheal Isaac <isaacmicheal600@gmail.com>
2023-11-07 18:29:25 -0800

    My advance javascript file

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+// create a function that will be used to send a message in the browser using alert
+function welcomeMessage(fullName) {
+  alert("welcome " + fullName);
+}
+
+let Isaac = welcomeMessage("Isaac");
+let Micheal = welcomeMessage("Micheal");
+let Omeiza = welcomeMessage("Omeiza");
